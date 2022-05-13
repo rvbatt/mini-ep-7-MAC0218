74bf88cda126628ac21922f0ad59a51cc56eb79f
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2022-05-13 12:50:14 -0300

    Implement AnalisadorDeAprovacao
    This finishes the assignment
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,20 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+	private lateinit var criterio: CriterioDeAprovacao
 
+	fun defineCriterio(criterio: CriterioDeAprovacao) {
+		this.criterio = criterio
+	}
+
+	fun fechaBoletim(boletim: Boletim): BoletimFechado {
+		val mediaFinal: Double = criterio.mediaFinal(boletim)
+		val foiAprovado: Boolean = criterio.estaAprovado(boletim)
+
+		return BoletimFechado(boletim.mediaEPs, boletim.mediaMiniEPs,
+                              mediaFinal, foiAprovado)
+	}
 }
